08c6e9de1ca91ab803c7efe756c3e82bc155364f
AwaisMiskeen <160594692+AwaisMiskeen@users.noreply.github.com>
2025-09-06 16:59:55 +0500

    practice set of chapter 2

---
+++ PRACTICE_CHAP_02/PROBLEM_o6.py
@@ -0,0 +1,5 @@
+# Write a python program to calculate the square of a number entered by the user?
+
+a = int(input("Enter your favourite num: "))
+
+print("Square of you favourite number is: " ,a**2) #a*a
