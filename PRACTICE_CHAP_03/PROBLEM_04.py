c4926ad2234426246adc706dc633d1125f5111a1
AwaisMiskeen <160594692+AwaisMiskeen@users.noreply.github.com>
2025-09-07 16:25:35 +0500

    complete chapter_03

---
+++ PRACTICE_CHAP_03/PROBLEM_04.py
@@ -0,0 +1,5 @@
+# Replace the double space from problem 3 with single spaces?
+
+a = "Awais is a good  boy" #after good i use double space
+
+print(a.replace("  " , " "))
---
+++ PRACTICE_CHAP_03/PROBLEM_05.py
@@ -0,0 +1,6 @@
+# Write a program to format the following letter using escape sequence
+# characters.
+# letter = "Dear Sir, this python course is nice. Thanks!" ?
+
+letter = "Dear Sir,\n\tThis Pythone course is nice.\n\tThanks"#\n use for new line and \t use for tab
+print(letter)
